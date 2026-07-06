1c613dac8a4f43491f27da40dfa6a56a9c331410
wuqimiao <lphgosali@gmail.com>
2026-07-06 10:16:50 +0900

    add test for App class

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -7,8 +7,15 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class AppTest {
-    @Test public void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
-    }
+  @Test
+  public void appHasAGreeting() {
+    App classUnderTest = new App();
+    assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+  }
+
+  @Test
+  public void testAppReturnHelloWorld() {
+    App classUnderTest = new App();
+    assertEquals("Hello World", classUnderTest.getGreeting());
+  }
 }
